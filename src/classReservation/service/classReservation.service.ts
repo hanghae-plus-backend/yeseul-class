2dc5f64b24941795f55ed8286deb7e05c1360850
Tasha Han <tasha@xquant-ai.com>
2024-03-29 19:04:26 +0900

    feat: error if not target date

--- src/classReservation/service/classReservation.service.ts
+++ src/classReservation/service/classReservation.service.ts
@@ -27,49 +27,57 @@ import { ClassRepository } from '../repositories/class.repository';
         const queryRunner = this.dataSource.createQueryRunner();
         await queryRunner.connect();
         await queryRunner.startTransaction();
-        // 특강에 대한 정보를 가져옵니다.
-        // const selectedClass = await this.classRepository.findById(classId);
-        // if (!selectedClass) {
-        //     throw new NotFoundException('해당하는 클래스를 찾을 수 없습니다.');
-        // }
-        // TODO: 최대인원값 selectedClass 에서 가져오기
+            
+        const selectedClass = await this.classRepository.findById(classId);
+        if (!selectedClass) {
+            throw new NotFoundException('해당하는 클래스를 찾을 수 없습니다.');
+        }
+        //TODO: 최대인원값 selectedClass 에서 가져오기
+        //TODO: 날짜도 selectedClass 에서 가져오기
+        const maxStudent = selectedClass.max_student_num
+
+        const currentDate = new Date();
+        const targetDate = selectedClass.class_signup_date
+        if (currentDate < targetDate) {
+            throw new HttpException('클래스 오픈 일은 000입니다', HttpStatus.BAD_REQUEST)
+        }
         
         try {
 
-        //예약 이미 했는지 확인
-        const existingReservation = await this.classReservationRepository.findUserReservation(
-            classId, userId) // 비관적 락 적용
-        if (existingReservation) {
-            // TODO: 실패 케이스에도 is_success:false 결과값 저장
-            throw new HttpException('중복 예약은 불가능합니다', HttpStatus.BAD_REQUEST)
-        }
-        // 예약갯수 30개 이상이면 불가. 근데 여기까지 하고 이후에 따른거랑 겹쳐서 여러명이 될 수도 있잖아? 락을 걸어야 할듯?
-        const currentlyReservedSeats = await this.classReservationRepository.findByClass(classId)
-        if (currentlyReservedSeats.length>29) {
-            // TODO: 실패 케이스에도 is_success:false 결과값 저장
-            throw new HttpException('해당 클래스는 정원이 가득찼습니다. 감사합니다.',HttpStatus.BAD_REQUEST)
-        }
+            //예약 이미 했는지 확인
+            const existingReservation = await this.classReservationRepository.findUserReservation(
+                classId, userId) // 비관적 락 적용
+            if (existingReservation) {
+                // TODO: 실패 케이스에도 is_success:false 결과값 저장
+                throw new HttpException('중복 예약은 불가능합니다', HttpStatus.BAD_REQUEST)
+            }
+            // 예약갯수 30개 이상이면 불가. 근데 여기까지 하고 이후에 따른거랑 겹쳐서 여러명이 될 수도 있잖아? 락을 걸어야 할듯?
+            const currentlyReservedSeats = await this.classReservationRepository.findByClass(classId)
+            if (currentlyReservedSeats.length>=maxStudent) {
+                // TODO: 실패 케이스에도 is_success:false 결과값 저장
+                throw new HttpException('해당 클래스는 정원이 가득찼습니다. 감사합니다.',HttpStatus.BAD_REQUEST)
+            }
 
-        //모든 유효성 검사 비즈니스 로직을 통과하면
-        const reservation = await this.classReservationRepository.createNew({
-            class_id :classId,
-            user_id : userId,
-            is_success: true
+            //모든 유효성 검사 비즈니스 로직을 통과하면
+            const reservation = await this.classReservationRepository.createNew({
+                class_id :classId,
+                user_id : userId,
+                is_success: true
 
-        })
-        // 트랜잭션을 커밋
-        await queryRunner.commitTransaction();
-        console.log('특강 신청 성공');
-        console.log(reservation);
-        return reservation
+            })
+            // 트랜잭션을 커밋
+            await queryRunner.commitTransaction();
+            console.log('특강 신청 성공');
+            console.log(reservation);
+            return reservation
         } catch (error) {
-        console.error('오류 발생: 트랜잭션 롤백, 다시 신청해주세요');
-        await queryRunner.rollbackTransaction();
-        throw error;
+            console.error('오류 발생: 트랜잭션 롤백, 다시 신청해주세요');
+            await queryRunner.rollbackTransaction();
+            throw error;
         } finally {
         // 쿼리 러너를 해제
-        console.log('Query Runner successfully disconnected')
-        await queryRunner.release();
+            console.log('Query Runner successfully disconnected')
+            await queryRunner.release();
         }
     }
 
